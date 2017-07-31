59e1c939dfd18fd107b0562163dedfc8e6a24b10
Doug Beney <dougbeney@Dougs-MacBook-Pro.local>
2017-07-31 12:40:16 -0400

    More progress

---
+++ static/js/physics.js
@@ -0,0 +1,18 @@
+---
+---
+
+/*P5*/
+
+{% include_relative _lib2/p5.js %}
+
+/*Matter.js*/
+
+{% include_relative _lib2/matter.js %}
+
+/*_BOX*/
+
+{% include_relative _lib2/_BOX.js %}
+
+/*_physicsMagic*/
+
+{% include_relative _lib2/_physicsMagic.js %}
